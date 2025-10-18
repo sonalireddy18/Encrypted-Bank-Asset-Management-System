b3cbc75feadf37c8cce25057455877fb081b8aba
Sonali <msonalireddy@gmail.com>
2025-10-18 17:59:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,33 @@
 #include<vector>
 #include <climits>
 #include <algorithm>
+#include "accounts.h"
+#include "signup.h"
 
 int main(){
+    UserRegistration registeration;
+    int ch;
+    cout<< "================================"<<endl;
+    cout<< "Welcome to bams"<<endl;
+    cout<< "================================"<<endl;
+    cout<< "1. Register\n2. login\n3. exit"<<endl;
+    cout<< "enter your choice: ";
+    cin>>ch;
+    switch(ch){
+        case 1:{
+            
+        }
+        case 2:{
 
+        }
+        case 3:{
+          return 0;
+
+        }
+        default: {
+            cout<<"invalid entry try again."<<endl;
+        }
+    }
 }
+
 
