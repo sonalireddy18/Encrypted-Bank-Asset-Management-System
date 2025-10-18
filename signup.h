f3bb8d31fca7e34905054036404b83aae82763f5
Sonali <msonalireddy@gmail.com>
2025-10-18 17:29:27 +0530

    Update signup.h

--- signup.h
+++ signup.h
@@ -1,1 +1,20 @@
+#ifndef SIGNUP_H
+#define  SIGNUP_H
+
+class Customer{
+private:
+string password ;
+public:
+string name;
+string username;
+int phno;
+Customer();
+~Customer();
+
+
+};
+
+
+
+#endif
 
