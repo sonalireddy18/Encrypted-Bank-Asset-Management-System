30cb709ddd3622210ddb4493f52afb9ad0382b67
nella140 <vootada.vennela@gmail.com>
2025-10-18 17:29:41 +0530

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,2 +1,39 @@
-//hello
+#include <string>
+using namespace std;
 
+#include <string>
+using namespace std;
+
+class Account
+{
+private:
+    string accountNumber;
+    double balance;
+
+public:
+    // Constructor
+    Account(const string& accNum, double bal)
+    {
+        accountNumber = accNum;
+        balance = bal;
+    }
+
+    void setBal(double bal)
+    {
+        balance = bal;
+    }
+};
+
+
+class SavingsAccount: protected Account
+{
+private:
+    double interestRate;
+
+public:
+    // Constructor
+    SavingsAccount(const string& accNum, double bal, double rate): Account(accNum, bal)
+    {
+        interestRate = rate;
+    }
+};
