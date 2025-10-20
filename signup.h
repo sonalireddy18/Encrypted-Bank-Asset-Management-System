7fbd101041a0681845b42e87ded4b161b616761e
Sonali <msonalireddy@gmail.com>
2025-10-20 14:42:03 +0530

    Update signup.h

--- signup.h
+++ signup.h
@@ -1,20 +1,65 @@
 #ifndef SIGNUP_H
-#define  SIGNUP_H
+#define SIGNUP_H
 
-class Customer{
+#include <string>
+#include <unordered_map>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+class Customer {
 private:
-string password ;
+    string passwordHash;  // Store hash instead of plain password
+    string bankAccountNo;
+    bool isLocked;
+    time_t lockTime;
+    
+
 public:
-string name;
-string username;
-int phno;
-Customer();
-~Customer();
-
-
+    int failedAttempts;
+    string name;
+    string username;
+    string phno;
+    
+    Customer();
+    
+    void setPassword(string pass);
+    bool verifyPassword(string pass) const;
+    bool isAccountLocked() const;
+    void incrementFailedAttempts();
+    void resetFailedAttempts();
+    void setBankAccountNo(string accNo);
+    string getBankAccountNo() const;
+    
+    // Password hashing function
+    static string hashPassword(const string& password); // so that nobody gets to know ur password.
+    
+    // For file operations
+    friend ofstream& operator<<(ofstream& ofs, const Customer& cust);
+    friend ifstream& operator>>(ifstream& ifs, Customer& cust);
 };
 
+class UserRegistration {
+private:
+    unordered_map<string, Customer> users;
+    
+    bool isValidPassword(const string& password) const;
+    bool isValidName(const string& name) const;
+    bool isValidPhone(const string& phone) const;
+    bool isUsernameTaken(const string& username) const;
 
+public:
+    UserRegistration();
+    ~UserRegistration();
+    
+    bool registerUser();
+    bool loginUser();
+    void saveToFile() const;
+    void loadFromFile();
+};
 
 #endif
-
