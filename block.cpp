953112f06bd8e91c9d7522447f80ac0bb75dd2a2
Sonali <msonalireddy@gmail.com>
2025-11-05 20:49:50 +0530

    Update and rename security.cpp to block.cpp

---
+++ block.cpp
@@ -0,0 +1,87 @@
+#include "block.h"
+#include <sstream>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+// Simple DJB2 hash function for strings
+static string hashString(const string& input) {
+    unsigned int hash = 5381;
+    for (char c : input) {
+        hash = ((hash << 5) + hash) + static_cast<unsigned char>(c); // hash * 33 + c
+    }
+    stringstream ss;
+    ss << hex << hash;
+    return ss.str();
+}
+
+// Calculate the hash representing the block
+string Block::calculateHash() const {
+    stringstream ss;
+    ss << index << previousHash << timestamp;
+    for (const auto& tx : transactions) {
+        ss << tx.fromAccount << tx.toAccount << tx.amount << tx.timestamp;
+    }
+    return hashString(ss.str());
+}
+
+Block::Block(int idx, const string& prevHash)
+    : index(idx), previousHash(prevHash), timestamp(time(nullptr)) {
+    hash = calculateHash();
+}
+
+void Block::addTransaction(const string& fromAcc, const string& toAcc, double amount) {
+    TransactionRecord tx{fromAcc, toAcc, amount, time(nullptr)};
+    transactions.push_back(tx);
+    // Update hash after adding transaction
+    hash = calculateHash();
+}
+
+string Block::getHash() const {
+    return hash;
+}
+
+string Block::getPreviousHash() const {
+    return previousHash;
+}
+
+int Block::getIndex() const {
+    return index;
+}
+
+void Block::printBlock() const {
+    cout << "Block #" << index << "\n";
+    cout << "Previous Hash: " << previousHash << "\n";
+    cout << "Hash: " << hash << "\n";
+    cout << "Timestamp: " << ctime(&timestamp);
+    cout << "Transactions:\n";
+    for (const auto& tx : transactions) {
+        cout << "  From: " << tx.fromAccount << ", To: " << tx.toAccount << ", Amount: " << tx.amount << ", Time: " << ctime(&tx.timestamp);
+    }
+    cout << "--------------------------------\n";
+}
+
+// Blockchain class maintaining chain of blocks
+Blockchain::Blockchain() {
+    // Create genesis block
+    chain.push_back(Block(0, "0"));
+}
+
+Block& Blockchain::getLatestBlock() {
+    return chain.back();
+}
+
+void Blockchain::addTransaction(const string& fromAcc, const string& toAcc, double amount) {
+    // Add transaction in new block: For simplicity, new block per transaction
+    Block newBlock(chain.size(), getLatestBlock().getHash());
+    newBlock.addTransaction(fromAcc, toAcc, amount);
+    chain.push_back(newBlock);
+}
+
+void Blockchain::printChain() const {
+    cout << "Blockchain contents:\n";
+    for (const auto& block : chain) {
+        block.printBlock();
+    }
+}
