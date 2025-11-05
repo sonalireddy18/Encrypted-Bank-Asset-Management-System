a78b6f269f262ebb07b4d74a3e4e1b51aaac68b3
Sonali <msonalireddy@gmail.com>
2025-11-05 20:53:45 +0530

    Update signup.h

--- signup.h
+++ signup.h
@@ -2,62 +2,42 @@
 #define SIGNUP_H
 
 #include <string>
-#include <unordered_map>
-#include <ctime>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <memory>
-#include "Accounts.h"
-using namespace std;
+#include <map>
+#include "account.h"
+
+const int MAX_FAILED_ATTEMPTS = 3;
+const int LOCK_DURATION = 300; // 5 minutes in seconds
 
 class Customer {
-private:
-    string passwordHash;  
-    string bankAccountNo;
-    bool isLocked;
-    time_t lockTime;
 public:
-    int failedAttempts;
-    string name;
-    string username;
-    string phno;
-    unique_ptr<BasicAccount> account;
-
     Customer();
-    void setPassword(string pass);
-    bool verifyPassword(string pass) const;
-    bool isAccountLocked() const;
-    void incrementFailedAttempts();
-    void resetFailedAttempts();
-    void setBankAccountNo(string accNo);
-    string getBankAccountNo() const;
+    void setPassword(std::string pass);
+    bool checkPassword(const std::string& pass);
+    std::string getBankAccountNo() const;
 
-    // Encryption + Hashing
-    static string hashPassword(const string& password);
-    static string encryptDecrypt(const string& data);
+    std::string passwordHash;
+    Account* account;
 
-    // File operations
-    friend ofstream& operator<<(ofstream& ofs, const Customer& cust);
-    friend ifstream& operator>>(ifstream& ifs, Customer& cust);
-};
-class UserRegistration {
+    bool isLocked;
+    int failedAttempts;
+    time_t lockTime;
+
 private:
-    unordered_map<string, Customer> users;
-    bool isValidPassword(const string& password) const;
-    bool isValidName(const string& name) const;
-    bool isValidPhone(const string& phone) const;
-    bool isUsernameTaken(const string& username) const;
-    void showDashboard(Customer& customer);
-public:
-    UserRegistration();
-    ~UserRegistration();
-
-    bool registerUser();
-    bool loginUser();
-    void saveToFile() const;
-    void loadFromFile();
+    static std::string hashPassword(const std::string& password);
+    static std::string encryptDecrypt(const std::string& data);
 };
 
-#endif
+class UserRegistration {
+public:
+    bool registerUser(const std::string& username, const std::string& password, const std::string& accNo);
+    bool authenticateUser(const std::string& username, const std::string& password);
+    bool deleteUserByUsername(const std::string& username);
+
+    Customer* getCustomerByUsername(const std::string& username);
+    Customer* getCustomerByAccountNo(const std::string& accNo);
+
+private:
+    std::map<std::string, Customer> users;
+};
+
+#endif // SIGNUP_H
