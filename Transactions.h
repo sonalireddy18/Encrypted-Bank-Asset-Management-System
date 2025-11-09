c56b6f523b6540c14edb30feeeb17be9e7d46c6b
Sonali <msonalireddy@gmail.com>
2025-11-09 22:16:26 +0530

    Update Transactions.h

--- Transactions.h
+++ Transactions.h
@@ -12,8 +12,12 @@ public:
     static void transferByAccountNoUserRegistration(UserRegistration& userReg, const std::string& fromAccNo, const std::string& toAccNo, double amount);
     static void withdrawUserRegistration(UserRegistration& userReg, const std::string& accNo, double amount);
     static void depositUserRegistration(UserRegistration& userReg, const std::string& accNo, double amount);
-    static void printTransactionHistory();
 
+    // Shows only current user's transactions
+    static void printTransactionHistory(const std::string& accNo);
+
+    // show entire blockchain ledger
+    static void printFullBlockchain();
 
     static void saveBlockchainToFile(const std::string& filename);
     static void loadBlockchainFromFile(const std::string& filename);
